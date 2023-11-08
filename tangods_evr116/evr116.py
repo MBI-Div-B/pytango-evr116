a5585b35f78e2b5aa1711f40b5ada618795df686
lrlunin <lunin@campus.tu-berlin.de>
2023-11-08 16:36:04 +0100

    moved dac calibration to property, bumbed version

--- tangods_evr116/evr116.py
+++ tangods_evr116/evr116.py
@@ -1,16 +1,29 @@
 from tango import AttrWriteType, DispLevel, DevState
-from tango.server import Device, attribute, command, device_property
+from tango.server import Device, attribute, command, device_property, GreenMode
 from importlib.resources import files
 import numpy as np
+import asyncio
 
 
 class EVR116Controller(Device):
+    DAC_DUTY_CYCLE_VALUES = device_property(
+        dtype=(float,),
+        doc="duty cycle values",
+        default_value=np.array([0, 10, 20, 30, 40, 50, 60, 70, 80, 90]),
+    )
+    DAC_VOLTAGES_VALUES = device_property(
+        dtype=(float,),
+        doc="measured values for the given duty cycle values",
+        default_value=np.array(
+            [0, 1.4, 2.77, 4.11, 5.45, 6.76, 8.07, 9.36, 10.44, 10.61]
+        ),
+    )
     voltage = attribute(
         label="appl. voltage",
         dtype=float,
         access=AttrWriteType.READ_WRITE,
         unit="V",
-        format="%.2f",
+        format="%2.2f",
         max_value=10,
         min_value=0,
     )
@@ -20,9 +33,40 @@ class EVR116Controller(Device):
         dtype=float,
         access=AttrWriteType.READ_WRITE,
         unit="mbar l/s",
-        format="%.2f",
+        format="%5.2f",
         min_value=0,
     )
+    percentage = attribute(
+        label="percent of flow",
+        dtype=float,
+        access=AttrWriteType.READ_WRITE,
+        unit="%",
+        format="%2.2f",
+        min_value=0,
+        max_value=100,
+    )
+
+    readme = attribute(
+        label="README",
+        dtype=str,
+        display_level=DispLevel.EXPERT,
+        doc="""You need to do the following steps to calibrate the DAC:
+    1) Disconnect the VakuPI cable from the valve.
+    2) Connect the "+" of voltmeter measure device to the
+    3d pinout of the cable (bottom middle)
+    3) Connect the "-" of voltmeter to any
+    neighbour pin 2nd or 4th (they both are GND)
+    4) After you start the \"dac_calibration\" command
+    the DAC's duty cycle will iterate in range of 0 to 90 (incl.)
+    with step of 10. Each step will take 6 seconds. You have to
+    remember the measured voltages
+    5) When calibration is done set the measured values
+    into DAC_VOLTAGES_VALUES property and restart the device.
+    6) Ensure that the voltages do not overshoot the 10V. It can cause damages to the valve.""",
+    )
+
+    def read_readme(self):
+        return "READ BEFORE ANY FURTHER STEPS"
 
     def read_voltage(self):
         return self._voltage
@@ -36,17 +80,37 @@ class EVR116Controller(Device):
     def write_gasflow(self, value):
         self.update_attrs(gasflow=value)
 
-    def update_attrs(self, voltage=None, gasflow=None):
-        if voltage is not None and gasflow is None:
+    def read_percentage(self):
+        return self._percentage
+
+    def write_percentage(self, value):
+        self.update_attrs(percentage=value)
+
+    def update_attrs(self, voltage=None, gasflow=None, percentage=None):
+        if voltage is not None and gasflow is None and percentage is None:
             self._voltage = voltage
             conv_gasflow = np.interp(
                 voltage, self._curve_voltage, self._curve_pressure, left=0
             )
             self._gasflow = conv_gasflow
-        elif gasflow is not None and voltage is None:
+            self._percentage = conv_gasflow / self._max_gas_flow * 100
+        elif gasflow is not None and voltage is None and percentage is None:
             self._gasflow = gasflow
+            self._percentage = gasflow / self._max_gas_flow * 100
             conv_voltage = np.interp(
                 gasflow, self._curve_pressure, self._curve_voltage, left=0, right=10
+            )
+            self._voltage = conv_voltage
+        elif percentage is not None and voltage is None and gasflow is None:
+            self._percentage = percentage
+            conv_gasflow = percentage / 100 * self._max_gas_flow
+            self._gasflow = conv_gasflow
+            conv_voltage = np.interp(
+                conv_gasflow,
+                self._curve_pressure,
+                self._curve_voltage,
+                left=0,
+                right=10,
             )
             self._voltage = conv_voltage
         else:
@@ -68,11 +132,27 @@ class EVR116Controller(Device):
             self.write_voltage(0)
             self.apply()
 
-    def init_valve(self):
-        from rpi_hardware_pwm import HardwarePWM
+    @command(display_level=DispLevel.EXPERT)
+    def dac_calibration(self):
+        self._command_loop.run_in_executor(None, self._dac_calibration)
 
-        self._hardware_pwm = HardwarePWM(pwm_channel=0, hz=1_000)
-        self._hardware_pwm.start(0)
+    def _dac_calibration(self):
+        import time
+
+        self.set_state(DevState.MOVING)
+        for duty_cycle in range(0, 100, 10):
+            # self._hardware_pwm.duty_cycle(duty_cycle)
+            self.info_stream(f"Duty cycle: {duty_cycle}")
+            time.sleep(6)
+        # self._hardware_pwm.duty_cycle(0)
+        self.set_state(DevState.ON)
+
+    def init_valve(self):
+        # from rpi_hardware_pwm import HardwarePWM
+
+        # self._hardware_pwm = HardwarePWM(pwm_channel=0, hz=1_000)
+        # self._hardware_pwm.start(0)
+        pass
 
     def delete_valve(self):
         self._hardware_pwm.duty_cycle(0)
@@ -99,19 +179,19 @@ class EVR116Controller(Device):
 
         # load DAC calibration curve for converting PWM duty cycle parameter of PWM
         # since the DAC is not linearly mapping 0..100 duty cycle to 0..10.61 Volts
-        voltage_curve_path = files("tangods_evr116.calibration").joinpath(
-            "duty_cycle_voltage_curve.csv"
-        )
-        cycle, voltage = np.loadtxt(voltage_curve_path, delimiter=";", unpack=True)
-        args = np.argsort(cycle)
-        self._duty_cycle, self._voltage_duty_cycle = cycle[args], voltage[args]
+        duty_cycle, voltage = self.DAC_DUTY_CYCLE_VALUES, self.DAC_VOLTAGES_VALUES
+        args = np.argsort(duty_cycle)
+        self._duty_cycle, self._voltage_duty_cycle = duty_cycle[args], voltage[args]
 
     def init_device(self):
         Device.init_device(self)
         self.load_calibration_curves()
         self._voltage = 0
         self._gasflow = 0
+        self._percentage = 0
+        self._max_gas_flow = np.max(self._curve_pressure)
         self.init_valve()
+        self._command_loop = asyncio.new_event_loop()
         self.set_state(DevState.ON)
 
     def delete_device(self):
